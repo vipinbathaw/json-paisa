dbd963e8766d7b8a2a0e801428c340b91531d0d0
Vipin Bathaw <gamesforvipin@gmail.com>
2023-05-03 00:18:31 +0530

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './styles/app.css';
 
 function App() {
+  const [item, setItem] = useState('');
+  const [value, setValue] = useState('');
+  const [type, setType] = useState('credit');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>Fin</h1>
       </header>
+      <div className="container">
+        <form>
+          <div className="row">
+            <input
+              value={item}
+              onChange={(e) => setItem(e.target.value)}
+              type="text"
+              placeholder="Item"
+            />
+          </div>
+          <div className="row">
+            <input
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              type="text"
+              placeholder="Value"
+            />
+          </div>
+          <div className="row">
+            <select value={type} onChange={(e) => setType(e.target.value)}>
+              <option value="debit">Debit</option>
+              <option value="credit">Credit</option>
+            </select>
+          </div>
+          <div className="row">
+            <button>add</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
