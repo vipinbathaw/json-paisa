8084fca788f58272be383277b106cc6049c37e2a
Vipin Bathaw <gamesforvipin@gmail.com>
2023-05-22 23:35:05 +0530

    daily sums and tables in report

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,10 +1,12 @@
 import Overview from './Overview';
+import DailySums from './Reports/DailySums';
 import PopularTags from './Reports/PopularTags';
 
 const Reports = (props) => {
   return (
     <div className="home">
       <Overview data={props.data} />
+      <DailySums data={props.data} />
       <PopularTags data={props.data} />
     </div>
   );
--- src/components/Reports/DailySums.js
+++ src/components/Reports/DailySums.js
@@ -1,36 +1,40 @@
 import { useState } from 'react';
 import { TX_TYPE } from '../../configs';
 
-const PopularTags = (props) => {
+const DailySums = (props) => {
   const [collapsed, setCollapsed] = useState(false);
 
-  let popularTags = {};
+  let sums = [];
+  let dates = {};
 
-  for (let i = 0; i < props.data.ledger.length; i++) {
-    let entry = props.data.ledger[i];
-    // for popular tags
-    for (let j = 0; j < entry.tags.length; j++) {
-      let tag = entry.tags[j];
-      if (popularTags[tag] === undefined) {
-        popularTags[tag] = { credit: 0, debit: 0, count: 0 };
-      }
-      if (entry.type === TX_TYPE.CREDIT) {
-        popularTags[tag].credit += parseFloat(entry.value);
-      } else {
-        popularTags[tag].debit += parseFloat(entry.value);
-      }
-      popularTags[tag].count += 1;
+  for (let j = 0; j < props.data.ledger.length; j++) {
+    let i = null;
+    let entry = props.data.ledger[j];
+
+    if (entry.date in dates) {
+      i = dates[entry.date];
+    } else {
+      sums.push({ date: entry.date, credit: 0, debit: 0, count: 0 });
+      i = sums.length - 1;
+      dates[entry.date] = i;
     }
+
+    if (entry.type === TX_TYPE.CREDIT) {
+      sums[i].credit += entry.value;
+    }
+    if (entry.type === TX_TYPE.DEBIT) {
+      sums[i].debit += entry.value;
+    }
+
+    sums[i].count += 1;
   }
 
-  const popularTagsArray = Object.entries(popularTags)
-    .map(([key, { credit, debit, count }]) => ({ key, credit, debit, count }))
-    .sort((a, b) => b.credit + b.debit - (a.credit + a.debit));
+  console.log({ sums });
 
   return (
     <div className="box">
       <div className="row row-v-center">
-        <h2>Popular Tags</h2>
+        <h2>Daily Sums</h2>
         <div
           className="collapser right"
           onClick={() => setCollapsed(!collapsed)}
@@ -43,7 +47,7 @@ const PopularTags = (props) => {
           <thead>
             <tr>
               <th>
-                <h5>Tag</h5>
+                <h5>Date</h5>
               </th>
               <th>
                 <h5>Count</h5>
@@ -57,19 +61,19 @@ const PopularTags = (props) => {
             </tr>
           </thead>
           <tbody>
-            {popularTagsArray.map((tag, i) => (
+            {sums.map((entry, i) => (
               <tr key={i}>
                 <td>
-                  <span>{tag.key}</span>
+                  <span>{entry.date}</span>
                 </td>
                 <td>
-                  <span>{tag.count}</span>
+                  <span>{entry.count}</span>
                 </td>
                 <td>
-                  <span className="credit">{tag.credit}</span>
+                  <span className="credit">{entry.credit}</span>
                 </td>
                 <td>
-                  <span className="debit">{tag.debit}</span>
+                  <span className="debit">{entry.debit}</span>
                 </td>
               </tr>
             ))}
@@ -80,4 +84,4 @@ const PopularTags = (props) => {
   );
 };
 
-export default PopularTags;
+export default DailySums;
